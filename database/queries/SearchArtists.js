deb6c8b6567a3f2f9f7dc3341c129f3e1661e8ac
Alex <oknerbob@gmail.com>
2018-01-31 10:36:12 +0200

    first commit - project working

---
+++ database/queries/SearchArtists.js
@@ -0,0 +1,39 @@
+const Artist = require('../models/artist');
+
+/**
+ * Searches through the Artist collection
+ * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {string} sortProperty The property to sort the results by
+ * @param {integer} offset How many records to skip in the result set
+ * @param {integer} limit How many records to return in the result set
+ * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ */
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = {};
+  if (criteria.name.length > 0) {
+    query['$text'] = {'$search': criteria.name, $caseSensitive: false};
+  }
+  if (criteria.age) {
+    query.age = {$gte: criteria.age.min, $lte: criteria.age.max};
+  }
+  if (criteria.yearsActive) {
+    query.yearsActive = {$gte: criteria.yearsActive.min, $lte: criteria.yearsActive.max};
+  }
+
+  const artistsFilter = Artist.find(query)
+    .sort({[sortProperty]: 1})
+    .skip(offset)
+    .limit(limit);
+
+  const artistsFilterCount = Artist.find(query).count();
+
+  return Promise.all([artistsFilter, artistsFilterCount])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit
+      }
+    })
+};
